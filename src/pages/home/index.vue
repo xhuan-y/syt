5c3aa5edc7291d7afe494101ecc6e1f988598226
xiaohuan <1305584373@qq.com>
2025-10-01 21:02:54 +0800

    首页等级与地区组件拆分与静态搭建

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Carousel from "./carousel/index.vue";
 import Search from "./search/index.vue";
+import Level from "./level/index.vue";
+import Region from "./region/index.vue";
 </script>
 
 <template>
@@ -9,6 +11,16 @@ import Search from "./search/index.vue";
     <Carousel />
     <!-- 首页搜索医院的表单区域 -->
     <Search />
+    <!-- 底部展示医院的结构 -->
+    <el-row gutter="20">
+      <el-col :span="20">
+        <!-- 等级子组件 -->
+        <Level />
+        <!-- 地区 -->
+        <Region />
+      </el-col>
+      <el-col :span="4">456</el-col>
+    </el-row>
   </div>
 </template>
 

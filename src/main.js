6686c0397cfcca5d1542ad916a7671379c0673a5
xiaohuan <1305584373@qq.com>
2025-10-02 15:27:54 +0800

    首页医院组件的拆分与静态搭建

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import "@/style/reset.scss";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 import App from "@/App.vue";
 import HospitalTop from "@/components/hospital_top/index.vue";
 import HospitalBottom from "@/components/hospital_bottom/index.vue";
@@ -10,6 +11,8 @@ import router from "@/router";
 const app = createApp(App);
 app.component("HospitalTop", HospitalTop);
 app.component("HospitalBottom", HospitalBottom);
-app.use(ElementPlus);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
 app.use(router);
 app.mount("#app");
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,6 +3,11 @@ import Carousel from "./carousel/index.vue";
 import Search from "./search/index.vue";
 import Level from "./level/index.vue";
 import Region from "./region/index.vue";
+import Card from "./card/index.vue";
+import { ref } from "vue";
+
+let pageNo = ref(1);
+let pageSize = ref(10);
 </script>
 
 <template>
@@ -18,10 +23,32 @@ import Region from "./region/index.vue";
         <Level />
         <!-- 地区 -->
         <Region />
+        <!-- 展示医院的结构 -->
+        <div class="hospital">
+          <Card class="item" v-for="item in 10" :key="item" />
+        </div>
+        <!-- 分页器 -->
+        <el-pagination
+          v-model:current-page="pageNo"
+          v-model:page-size="pageSize"
+          :page-sizes="[10, 20, 30, 40]"
+          layout="prev, pager, next, jumper, ->, sizes, total"
+          :total="13"
+        />
       </el-col>
       <el-col :span="4">456</el-col>
     </el-row>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.hospital {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  .item {
+    width: 49%;
+    margin: 10px 0;
+  }
+}
+</style>
